dca1f40974e90e5f2e77c6691130f036d8208265
Kaustubh22327 <clasherkaustubh@gmail.com>
2025-02-10 19:20:06 +0530

    more questions

--- B_Make_AP.cpp
+++ B_Make_AP.cpp
@@ -2,51 +2,9 @@
 using namespace std;
 
 void solve() {
-    int n;
-    cin>>n;
-    vector<int>vt(n);
-    int l,m,r=0;
-    for(int x=0;x<n;x++){
-        cin>>vt[x];
-    }
-    bool found=false;
-    for(int j=1;j<n-1;j++){
-        int cl,cm,cr=0;
-        cm=j;
-        bool leftfound=false;
-        bool rightfound=false;
-        //left mein check
-        for(int i=0;i<j;i++){
-            if(vt[i]<vt[j]){
-                leftfound=true;
-                cl=i;
-                break;
-            }    
-        }
-        //right mein check
-        for(int k=j+1;k<n;k++){
-            if(vt[k]<vt[j]){
-                rightfound=true;
-                cr=k;
-                break;
-            }    
-        }
-        found=leftfound&&rightfound;
-        if(found==true){
-            l=cl;
-            m=cm;
-            r=cr;
-            break;
-        }
-    }
-    if(found){
-        cout<<"YES"<<endl;
-        cout<<l+1<<" "<<m+1<<" "<<r+1<<endl;
-        
-    }
-    else{
-        cout<<"NO"<<endl;
-    }
+    int a,b,c;
+    cin>>a>>b>>c;
+    
 }
 int main(){
     ios::sync_with_stdio(false);
