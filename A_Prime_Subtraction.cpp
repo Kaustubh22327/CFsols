623db04f1ec19e47ce498d93e2a7fb4e04756163
Kaustubh22327 <clasherkaustubh@gmail.com>
2025-02-22 01:15:25 +0530

    more

--- A_Prime_Subtraction.cpp
+++ A_Prime_Subtraction.cpp
@@ -1,20 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    long long n;
-    cin>>n;
-    vector<int>vt(n+1,0);
-    for(int i=1;i<=n;i++){
-        int x;
-        cin>>x;
-        vt[x-1]=i;
-    }
-    for (int i=0; i<n;i++)
-    {
-        cout<<vt[i]<<" ";
-    }
-    
+    int t;
+    cin>>t;
+    while(t--){
+        long long x,y;
+        cin>>x>>y;
+        if(x-y==1){
+            cout<<"NO"<<endl;
+            
+        }
+        else{
+            cout<<"YES"<<endl;
+        }
 
+    }
     
+    return 0;
 
 }
