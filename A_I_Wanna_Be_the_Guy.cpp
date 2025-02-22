8675c2aa862fedcfce198fd638d28bcdd93dc6e9
Kaustubh22327 <clasherkaustubh@gmail.com>
2025-02-22 19:18:03 +0530

    more changes

--- A_I_Wanna_Be_the_Guy.cpp
+++ A_I_Wanna_Be_the_Guy.cpp
@@ -1,1 +1,29 @@
-#in
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    set<int>st;
+    int p;
+    cin>>p;
+    for(int i=0;i<p;i++){
+        int x;
+        cin>>x;
+        st.insert(x);
+    }
+    int q;
+    cin>>q;
+    for(int i=0;i<q;i++){
+        int x;
+        cin>>x;
+        st.insert(x);
+    }
+    if(st.size()==n){
+        cout<<"I become the guy."<<endl;
+    }
+    else{
+        cout<<"Oh, my keyboard!"<<endl;
+    }
+}
+
+//pangram
