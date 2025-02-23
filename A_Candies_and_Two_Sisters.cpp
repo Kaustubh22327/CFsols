59287ec12faf15ae277b5a24abeb7baf9d4d0f39
Kaustubh22327 <clasherkaustubh@gmail.com>
2025-02-23 13:25:09 +0530

    hell

---
+++ A_Candies_and_Two_Sisters.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<map>
+using namespace std;
+int main(){
+    string guest,host,pile;
+    cin>>guest>>host>>pile;
+    map<char,int> nameCount;
+    map<char,int> pileCount;
+    for(int i=0;i<guest.length();i++){
+        nameCount[guest[i]]++;
+    }
+    for(int i=0;i<host.length();i++){
+        nameCount[host[i]]++;
+    }
+    for(int i=0;i<pile.length();i++){
+        pileCount[pile[i]]++;
+    }
+    if(nameCount==pileCount){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
