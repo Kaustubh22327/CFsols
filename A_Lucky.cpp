7ae4522719986b258dc063b1fa9df6d5a509d3e4
Kaustubh22327 <clasherkaustubh@gmail.com>
2025-02-24 03:41:44 +0530

    more questions

--- A_Lucky.cpp
+++ A_Lucky.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-int t;cin>>t;
-while(t--){
-string s;cin>>s;
-int a=s[0]+s[1]+s[2],b=s[3]+s[4]+s[5];
-cout<<(a==b?"YES":"NO")<<"\n";
+    int t;
+    cin>>t;
+    while(t--){
+    string s;
+    cin>>s;
+    int a=s[0]+s[1]+s[2],b=s[3]+s[4]+s[5];
+    cout<<(a==b?"YES":"NO")<<"\n";
 }
