02c8ed0f56d17cf285023505d8aa0bad241d007c
Kaustubh22327 <clasherkaustubh@gmail.com>
2025-02-23 09:51:26 +0530

    more

---
+++ A_Anton_and_Letters.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin>>s;
+    cout<<s;
+    // set<char>st;
+    // for(int i=1;i<s.length()-1;i++){
+    //     st.insert(s[i]);
+    // }
+    // for (auto it = st.begin(); it != st.end(); ++it)
+    // cout << *it << " ";
+}
