d342184d181b5bd9bbc13bf9a26eac4f027ab6ea
Kaustubh22327 <clasherkaustubh@gmail.com>
2025-02-24 16:40:40 +0530

    more

---
+++ A_Anton_and_Polyhedrons.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n, faces = 0;
+    cin >> n;
+    string s;
+    while (n--){
+        cin >> s;
+        if (s =="Tetrahedron"){ 
+            faces+=4;
+        }
+        else if(s =="Cube"){ 
+            faces+=6;
+        }
+        else if(s =="Octahedron"){
+            faces+=8;
+        }
+        else if(s =="Dodecahedron"){
+            faces+=12;
+        }
+        else if(s == "Icosahedron"){
+            faces+=20;
+        }
+    cout<<faces<<endl;
+    return 0;
+}
