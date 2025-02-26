b7d4e6c401609b09bf19a4f6567169d8d3db14f5
Kaustubh22327 <clasherkaustubh@gmail.com>
2025-02-26 16:32:30 +0530

    l

---
+++ B_Sale.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, m;
+    cin >> n >> m;
+    vector<int>vt(n);
+    for(int i=0; i<n; i++) cin >> vt[i];
+    sort(vt.begin(), vt.end()); 
+    int sum=0;
+    for(int i=0;i<m;i++){
+        if(vt[i]<0){
+            sum+=vt[i];
+        }
+    }
+    cout<<abs(sum)<<endl;
+    return 0;
+}
