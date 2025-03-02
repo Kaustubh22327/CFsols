f5e04bcd9cf82d3c0db0bff56f18e23e74f9fecc
Kaustubh22327 <clasherkaustubh@gmail.com>
2025-03-02 11:55:59 +0530

    r

--- B_Food_Buying.cpp
+++ B_Food_Buying.cpp
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main(){
     int t;
     cin>>t;
-    while(t--){
-        int s;
+    while (t--){
+        long long s;
         cin>>s;
-        int ans=s;
-        while(s>=1){
-            int r=round(s/10);
-            r++;
-            cout<<r<< " ";
-            ans+=r;
-            s=r;
-        }
+        long long ans = 0;
+        while(s>=10){
+            s-=10;
+            ans+=10;
+            s+=1;
+        }     
+        ans+=s;  
         cout<<ans<<endl;
-
     }
+    
+    return 0;
 }
