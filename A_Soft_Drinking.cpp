921394872f80bd382e038e14b7f9abab9f4bfb2f
Kaustubh22327 <clasherkaustubh@gmail.com>
2025-03-07 13:57:07 +0530

    m

---
+++ A_Soft_Drinking.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int n,k,l,c,d,p,nl,np;
+   cin>>n>>k>>l>>c>>d>>p>>nl>>np;
+   int x=(k*l)/nl;
+   int y=c*d;
+   int z=p/np;
+   int mini=min({x,y,z});
+   cout<<mini/n<<endl;
+}
