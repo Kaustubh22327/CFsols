263f254b03d6df474ca5d7dd19a95cc19109ce9a
Kaustubh22327 <clasherkaustubh@gmail.com>
2025-03-07 13:56:54 +0530

    m

--- A_Anton_and_Polyhedrons.cpp
+++ A_Anton_and_Polyhedrons.cpp
@@ -1,26 +1,33 @@
 #include<iostream>
 using namespace std;
 int main() {
-    int n, faces = 0;
-    cin >> n;
-    string s;
-    while (n--){
-        cin >> s;
-        if (s =="Tetrahedron"){ 
-            faces+=4;
+    int n,sum;
+    string str;
+    while(cin>>n)
+    {
+        sum=0;
+        while(n--)
+        {
+            cin>>str;
+            if(str=="Tetrahedron"){
+                sum+=4;
+            }
+            else if(str=="Cube"){
+                sum+=6;
+            }
+            else if(str=="Octahedron"){
+                sum+=8;
+            }
+            else if(str=="Dodecahedron"){
+                sum+=12;
+
+            }
+            else if(str=="Icosahedron"){
+                sum+=20;
+
+            }
         }
-        else if(s =="Cube"){ 
-            faces+=6;
-        }
-        else if(s =="Octahedron"){
-            faces+=8;
-        }
-        else if(s =="Dodecahedron"){
-            faces+=12;
-        }
-        else if(s == "Icosahedron"){
-            faces+=20;
-        }
-    cout<<faces<<endl;
+        cout<<sum<<endl;
+    }
     return 0;
 }
