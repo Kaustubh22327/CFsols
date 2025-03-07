115d2979287f1db891b80019c596ee5af6c766f9
Kaustubh22327 <clasherkaustubh@gmail.com>
2025-03-07 19:47:45 +0530

    m

---
+++ A_Marathon.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        int cnt=0;
+        if(b>a){
+            cnt=cnt+1;
+        }
+        if(c>a){
+            cnt=cnt+1;
+        }
+        if(d>a){
+            cnt=cnt+1;
+        }
+        cout<<cnt<<endl;
+    }
+
+    return 0;
+}
