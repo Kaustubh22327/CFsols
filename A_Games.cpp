50fc1b8d2b1e54311c9544000023e8d0fc1e02b2
Kaustubh22327 <clasherkaustubh@gmail.com>
2025-03-09 18:06:15 +0530

    m

--- A_Games.cpp
+++ A_Games.cpp
@@ -15,7 +15,7 @@ int main()
    int ans=0;
    for(int i=0;i<n;i++){
     for(int j=0;j<n;j++){
-        if(i!=j && hcolor[i]==gcolor[j]){
+        if(hcolor[i]==gcolor[j]){
             ans++;
         }
     }
