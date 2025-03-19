3334e62b6c04bdf2be89801384f81e38b2a697f2
Kaustubh22327 <clasherkaustubh@gmail.com>
2025-03-19 23:21:01 +0530

    l

---
+++ A_Exciting_Bets.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;     
+    long long n;
+    while(t--){
+       int a,b;
+       cin>>a>>b;
+       int maxi=abs(a-b);
+       
+    }
+     return 0;
+}
