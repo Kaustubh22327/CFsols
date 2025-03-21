23d814cb7feca0f6e536e3f98b4a231feef4fa5f
Kaustubh22327 <clasherkaustubh@gmail.com>
2025-03-22 01:37:36 +0530

    m

---
+++ A_Vlad_and_the_Best_of_Five.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+      string s;
+      cin>>s;
+      int cnta=0;
+      int cntb=0;
+      for(int i=0;i<5;i++){
+        if(s[i]=='A'){
+            cnta++;
+        }
+        else{
+            cntb++;
+        }
+      }
+      if(cnta>cntb){
+        cout<<'A'<<endl;
+      }
+      else{
+        cout<<'B'<<endl;
+      }
+    }
+    return 0;
+}
