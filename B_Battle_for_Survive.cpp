9b39c10412c7368fb2d2a0fc94767e6689a47dd6
Kaustubh22327 <clasherkaustubh@gmail.com>
2025-03-29 00:45:34 +0530

    m

---
+++ B_Battle_for_Survive.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve() {
+    int n;
+    cin >> n;
+    vector<long long>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+
+    }
+    sort(a.begin(),a.end());
+    
+    cout<<a[n-1]<<endl;
+}
+int main(){
+    int t;
+    cin >>t;
+    while (t--){
+        solve();
+    }
+    
+    return 0;
+}
